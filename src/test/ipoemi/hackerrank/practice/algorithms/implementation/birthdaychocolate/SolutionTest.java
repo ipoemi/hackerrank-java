471e69fba4b7d57a2c32a256a0037c5aa45c4218
ipoemi <ipoemi@nate.com>
2018-04-24 15:05:19 +0900

    ~ day of the programmer added

---
+++ src/test/ipoemi/hackerrank/practice/algorithms/implementation/birthdaychocolate/SolutionTest.java
@@ -0,0 +1,18 @@
+package ipoemi.hackerrank.practice.algorithms.implementation.birthdaychocolate;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void solve() {
+        int sol = Solution.solve(5, new int[]{1, 2, 1, 3, 2}, 3, 2);
+        assertEquals(2, sol);
+        sol = Solution.solve(6, new int[]{1, 1, 1, 1, 1, 1}, 3, 2);
+        assertEquals(0, sol);
+        sol = Solution.solve(1, new int[]{4}, 4, 1);
+        assertEquals(1, sol);
+    }
+}
---
+++ src/test/ipoemi/hackerrank/practice/algorithms/implementation/divisiblesumpairs/SolutionTest.java
@@ -0,0 +1,14 @@
+package ipoemi.hackerrank.practice.algorithms.implementation.divisiblesumpairs;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void divisibleSumPairs() {
+        int sol = Solution.divisibleSumPairs(6, 3, new int[]{1, 3, 2, 6, 1, 2});
+        assertEquals(5, sol);
+    }
+}
