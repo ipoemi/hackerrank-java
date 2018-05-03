11f3b00e0f71ba6f86979f0e77a451a947b63951
ipoemi <ipoemi@nate.com>
2018-05-03 22:25:34 +0900

    Drawing Book Added

---
+++ src/test/ipoemi/hackerrank/practice/algorithms/implementation/drawingbook/SolutionTest.java
@@ -0,0 +1,17 @@
+package ipoemi.hackerrank.practice.algorithms.implementation.drawingbook;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void pageCount() {
+        int sol = Solution.pageCount(6, 2);
+        assertEquals(1, sol);
+
+        sol = Solution.pageCount(5, 4);
+        assertEquals(0, sol);
+    }
+}
