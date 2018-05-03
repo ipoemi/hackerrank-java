32ee4159bbc06617a7e9e9eb5c076a1f15e31684
ipoemi <ipoemi@nate.com>
2018-05-03 09:59:42 +0900

    Sock Merchant Added

---
+++ src/test/ipoemi/hackerrank/practice/algorithms/implementation/sockmerchant/SolutionTest.java
@@ -0,0 +1,14 @@
+package ipoemi.hackerrank.practice.algorithms.implementation.sockmerchant;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void sockMerchant() {
+        int sol = Solution.sockMerchant(9, new int[]{ 10, 20, 20, 10, 10, 30, 50, 10, 20 });
+        assertEquals(3, sol);
+    }
+}
